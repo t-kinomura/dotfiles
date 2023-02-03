8fe8edfce5ff2364a36ab3d72ceea88eef4b6389
Tetsu Kinomura <tetsu99soccer@gmail.com>
2023-02-03 00:15:18 +0000

    :+1: keymap tabnew

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -1,6 +1,7 @@
 vim.keymap.set("n", "<CR><CR>", "<C-w>w")
 vim.keymap.set("n", "<ESC><ESC>", ":nohlsearch<CR>")
 vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("n", "<leader>t", ":tabnew<CR>")
 
 -- lsp
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
