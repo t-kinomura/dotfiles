6993214780ab261f43a9189322bfb5266c761652
Tetsu Kinomura <tetsukinomura@gmail.com>
2024-04-17 13:10:45 +0900

    :package: Update telescope

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,7 +2,7 @@ vim.cmd.packadd "packer.nvim"
 
 return require('packer').startup(function(use)
 	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		'nvim-telescope/telescope.nvim', tag = '0.1.4',
 		requires = {
 			{ 'nvim-lua/plenary.nvim' },
 			{ 'nvim-treesitter/nvim-treesitter' },
